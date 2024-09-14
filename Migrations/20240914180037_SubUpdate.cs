e8e2cefa65b2f3dd2dd1c623cb70ad5f868e8849
ursapictura <hsmithfreelance@gmail.com>
2024-09-14 13:02:45 -0500

    Changes to Subscription model. Database Migration.

---
+++ Migrations/20240914180037_SubUpdate.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Rare_BE.Migrations
+{
+    public partial class SubUpdate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Subscriptions_Users_AuthorId",
+                table: "Subscriptions");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Subscriptions_AuthorId",
+                table: "Subscriptions");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_Subscriptions_AuthorId",
+                table: "Subscriptions",
+                column: "AuthorId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Subscriptions_Users_AuthorId",
+                table: "Subscriptions",
+                column: "AuthorId",
+                principalTable: "Users",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
