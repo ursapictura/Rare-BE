2440193f5000f9496328c9e7c6ce6b7b3e9a8aed
ursapictura <hsmithfreelance@gmail.com>
2024-09-13 19:49:45 -0500

    Get all posts from user's subsrcibed authors.

--- APIs/SubscriptionAPI.cs
+++ APIs/SubscriptionAPI.cs
@@ -1,9 +1,29 @@
-﻿namespace Rare.APIs
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Rare.APIs
 {
     public class SubscriptionAPI
     {
         public static void Map(WebApplication app)
         {
+            // Get User Subscriptions
+            app.MapGet("/subscribe/{userId}", (RareDbContext db, int userId) =>
+            {
+                var userSubscriptions = db.Subscriptions
+                .Include(s => s.Author)
+                .ThenInclude(a => a.Posts)
+                .Where(s => s.FollowerId == userId)
+                .Where(s => s.EndedOn == null)
+                .ToList();
+
+                if (!userSubscriptions.Any())
+                {
+                    return Results.Ok("No subscriptions found");
+                }
+
+                return Results.Ok(userSubscriptions);
+
+            });
 
         }
     }
